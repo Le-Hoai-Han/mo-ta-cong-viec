f66503496fe17308bdcc1b9607efe287dfcb2137
Le-Hoai-Han <hanhoai08091996@gmail.com>
2024-03-02 16:32:51 +0700

    cap nhat user

--- resources/views/front/vitri/show/_thong_tin_nhan_vien.blade.php
+++ resources/views/front/vitri/show/_thong_tin_nhan_vien.blade.php
@@ -2,7 +2,7 @@
         <td style="width: 10%;height: 185px;text-align: center"><img src="{{asset('storage/'. ($viTri->user != null ? $viTri->user->profile_photo_path :''))}}" alt="" height="185px" class="img-profile"></td>
         <td style="vertical-align: top;">
             <p class="text-thong-tin"> Họ và tên: {{$nhanVien != null ?$nhanVien->name :'Đang cập nhật'}} 
-                @if(auth()->user()->hasRole('Admin') || (auth()->user()->hasRole('mo_ta_cong_viec') && auth()->user()->isCapTren($viTri)))
+                @if(auth()->user()->hasRole('Admin'))
                 <a id="edit-user" style="<?php echo ($viTri->trang_thai != 0 ? 'display:none' :'') ?>">
                     <span class="material-icons" style="font-size: 20px;cursor: pointer;">
                         edit
